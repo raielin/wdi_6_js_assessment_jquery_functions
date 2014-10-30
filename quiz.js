cd8e6c5459fdf231c6690e8159fae96b5ae25bcb
raielin@gmail.com <raielin@gmail.com>
2014-10-30 11:41:51 -0400

    Ran out of time. Here's what I have.

--- quiz.js
+++ quiz.js
@@ -1,36 +1,87 @@
 // Do NOT modify the HTML document manually!!!!
+$(document).ready(function(){
 
 // Question 1
 // Use jQuery to add two more elements to the the UL in the 'question-1' div
 
+  // Checked jQuery documentation for syntax to select nested elements.
+  $('#question-1 > ul').append('<li>' + 'bird' + '</li>');
+  $('#question-1 > ul').append('<li>' + 'hamster' + '</li>');
+
 // Question 2
 // Add a click event handler to the paragraph in the 'question-2' div
-// which triggers a method called 'alertClicked', 
+// which triggers a method called 'alertClicked',
 // which will display an alert with the text of "Question 2 Success".
 
-var alertClicked = function() {
+  var alertClicked = function() {
+    alert("Question 2 Success");
+    $(this);
+  };
 
-};
+  $('#question-2 > p').on('click', alertClicked);
 
 // Question 3
-// Why won't the following code make clicking on anything the 'question-3' div 
+// Why won't the following code make clicking on anything the 'question-3' div
 // invoke the alertClicked method?
 
-$('#question-3').on('click', alertClicked);
+  $('#question-3').on('click', alertClicked);
+
+  // Because the div element itself has no direct content. Would need to
+  // select the h2 tag within the div in order for it to work. (I think?)
+
 
 // Question 4
 // Make the HTML in the 'question-4' div fade out when you hover over it,
 // and fade back in when you move your mouse away from it
 
+  var fade = function() {
+    $(this).hide('slow');
+  }
+
+  var appear = function() {
+    $(this).show('slow');
+  }
+
+  // Looked up what the exact event name was that I needed.
+  // Still can't get it to stay hidden until mouse moves away from it to fade back in
+  // $('#question-4 > h2').hover(fade);
+  $('#question-4 > h2').mouseover(fade);
+  $('#question-4 > h2').mouseout(appear);
+
+
 // Question 5
-// Make question 5's h2 turn blue when someone types 'winning' 
+// Make question 5's h2 turn blue when someone types 'winning'
 // in its input element and hits enter
+
+  // Used class notes from jQuery demo
+  // Searched for how to select an element with no ID name
+  // Can't get the input value to be evaluated
+  var input = $('#q-5-input').val();
+  if (input === "winning") {
+    console.log("test message")
+    $('#q-5-input').closest('h2').addClass('blue');
+  }
+  $('#q-5-input').val('');
 
 // Question 6
 // Add a method to the String prototype called 'reverse'
 // which will return the string reversed.
 // You should be able to run 'foo'.reverse() and get 'oof'
 // Use this method to reverse the text in the h2 in the 'question-6' div
+
+  String.prototype.reverse = function() {
+    var chars = [];
+    for (i = this.length - 1; i >= 0; i--) {
+      chars.push(this[i]);
+    };
+    return chars.join('');
+  };
+
+  $('#question-6 > h2').html('Question 6'.reverse());
+
+
+
+//   OUT OF TIME
 
 // Question 7
 // Write a method called 'highLightHeaders' that will add a class of 'highlight'
@@ -49,9 +100,12 @@ $('#question-3').on('click', alertClicked);
 // sum the prices of the objects in the array below.
 // Do *not* use a for loop
 
-var items = [
-  {name: 'iPhone 5c', price: 99.99},
-  {name: 'iPhone 5s', price: 149.99},
-  {name: 'iPhone 6', price: 249.99},
-  {name: 'iPhone 6 plus', price: 399.99}
-];
+  var items = [
+    {name: 'iPhone 5c', price: 99.99},
+    {name: 'iPhone 5s', price: 149.99},
+    {name: 'iPhone 6', price: 249.99},
+    {name: 'iPhone 6 plus', price: 399.99}
+  ];
+});
+
+
